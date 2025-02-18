0e963e1a47d6d4c5a39bd618ea9d32928a5539d9
Elias Gabriel <eliasgabrielcf@gmail.com>
2025-02-18 14:25:10 -0300

    04

--- Logger.js
+++ Logger.js
@@ -1,6 +1,10 @@
-class Logger {
+class SingletonLogger {
   constructor() {
+    if (SingletonLogger.instance) {
+      return SingletonLogger.instance
+    }
     this.logs = []
+    SingletonLogger.instance = this
   }
 
   log(message) {
@@ -13,4 +17,4 @@ class Logger {
   }
 }
 
-module.exports = Logger
+module.exports = SingletonLogger
--- testLogger.js
+++ testLogger.js
@@ -2,9 +2,12 @@ const Logger = require("./Logger")
 
 const logger1 = new Logger()
 const logger2 = new Logger()
+const logger3 = new Logger()
+const logger4 = new Logger()
+const logger5 = new Logger()
 
 logger1.log("Primeira mensagem")
 logger2.log("Segunda mensagem")
 
 logger1.print()
-logger2.print()
+logger5.print()
